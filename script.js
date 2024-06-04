70f91013ea1221aef141c1f293f4d5c335b686ab
Umidjon Mirzaaliyev Web <115705707+UmidjonMirzaaliyev@users.noreply.github.com>
2024-06-04 23:04:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,210 @@
+// 1-masala
+
+// let arr = []
+
+// for(let i = 0; i < 6; i++){
+//   let son = Math.floor(Math.random() * 10)
+
+//   if(arr.length < 6){
+//     arr.push(son)
+//   }
+
+// }
+// console.log(arr);
+
+// 2-masala
+
+// let elArr = []
+
+// elArr.push(25, 56, true, 99, 'baloon', false)
+
+// elArr.pop()
+
+// let strArr = elArr.toString()
+// console.log(strArr);
+
+//3-masala
+
+// let myArray = [];
+
+// myArray.unshift('bir');
+// myArray.unshift('ikki');
+// myArray.unshift('uch');
+// myArray.unshift("to'rt");
+
+// console.log("To'g'ri tartibdagi massiv", myArray);
+
+// myArray.reverse();
+
+// console.log('Teskari tartibdagi massiv', myArray);
+
+// let arrayLength = myArray.length;
+
+// console.log('Massivning uzunligi', arrayLength);
+
+//4-masala
+
+// let sonArr = []
+
+// sonArr.unshift(45);
+// sonArr.unshift(46);
+// sonArr.unshift(47);
+// sonArr.unshift(48);
+
+// sonArr.shift();
+
+// let res = sonArr.join(',')
+
+// console.log(res);
+
+// 5-masala
+
+// let royxat = [44, 'Umidjon', 17, 88, "Java Script", "Massiv", true]
+
+// royxat.pop()
+
+// royxatLength = royxat.length
+// let el1 = royxat[0]
+
+// console.log(royxatLength, el1);
+
+// 6-masala
+
+// let elArr = [44, 66, 'Mirzaaliyev', 'Java Script', false, 'Type script', true]
+
+// elArr.push(2007)
+
+// let oxirgiEl = elArr.at(7)
+
+// let strEl = oxirgiEl.toString()
+// console.log(strEl);
+
+// 7-masala
+
+// let elArr = [44, 66, 'Mirzaaliyev', 'Java Script', false, 'Type script', true]
+
+// elArr.unshift("BMW M5 CS")
+// elArr.reverse()
+// console.log(elArr);
+// console.log(elArr.length);
+
+// 8-masala
+
+// let elArr = [44, 66, 'Mirzaaliyev', 'Java Script', false, 'Type script', true]
+
+// elArr.shift()
+
+// let elLength = elArr.length
+// console.log(elLength);
+
+// let res = elArr.join(',')
+// console.log(res);
+
+// 9-masala
+
+// let elArr = [44, 66, "Mirzaaliyev", "Java Script", false, "Type script", true];
+
+// elArr.push("BMW M5 CS");
+// elArr.unshift("Chevrolet Malibu 2 PREMIER")
+// elArr.push("Coding time")
+
+// console.log(elArr.length);
+
+// elArr.reverse()
+// let res = elArr.join(',')
+// console.log(res);
+
+// 10-masala
+
+// let elArr = [44, 66, "Mirzaaliyev", "Java Script", false, "Type script", true];
+
+// elArr.pop();
+// elArr.unshift();
+
+// console.log(elArr.length);
+
+// let reverseEl = elArr.reverse()
+// console.log(reverseEl);
+
+// 11-masala
+
+// let elArr = [44, 66, "Mirzaaliyev", "Java Script", false, "Type script", true];
+
+// let reverseEll = elArr.reverse()
+
+// let strArr = reverseEll.toString()
+// console.log(strArr);
+// let strAllLength = strArr.length
+// console.log(strAllLength);
+
+// 12-masala
+
+// let arr1 = [1, 2, 3, 4, 5]
+// let arr2 = ['BMW M5 E60', 'BMW M5 F90 CS', 'M5 F10', 'BMW E39', 'BMW M3 GTR']
+
+// arr3 = arr1.concat(arr2)
+
+// arr3 = arr3.slice(2)
+// console.log(arr3.length);
+
+// 13-masala
+
+// let arr = ["apple", "banana", "orange", "grape", "kiwi"];
+
+// arr.splice(1, 3)
+
+// let strArr = arr.join(' ')
+// let strLength = strArr.length
+
+// console.log(strLength);
+
+// 14-masala
+
+// let arr = ["apple", "banana", "orange", "grape", "kiwi"];
+
+// arr = arr.slice(2)
+
+// let reverseArr = arr.reverse()
+// console.log(reverseArr);
+
+// 15-masala
+
+// let arr = [0, 1, [2, [3, [4, 5]]]];
+
+// arr = arr.flat(Infinity)
+
+// let arr2 = arr.at(5)
+// console.log(arr2);
+
+// 16-masala
+
+// let arr = [1, 2, 3, 4, 5];
+
+// let arr2 = arr.at(2)
+
+// arr.unshift(arr2)
+// console.log(arr);
+// console.log(arr.length);
+
+// 17-masala
+
+// let arr = ["apple", "banana", "orange", "grape", "kiwi"];
+// arr.reverse()
+
+// let strArr = arr.join(', ')
+// // console.log(strArr);
+
+// let oxirgiEl = arr.at(-1)
+// console.log(oxirgiEl);
+
+// 18-masala
+
+// let arr = ["apple", "banana", "orange", "grape", "kiwi"];
+
+// arr2 = arr.slice(0, 2);
+// // console.log(arr2);
+
+// arr.splice(3)
+// console.log(arr);
+
+// 19-masala
